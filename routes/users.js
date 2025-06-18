c8cff0adcae82d42d1c09428e8b4f668279c5cd4
Nahia Otegui <nay.fullstack@gmail.com>
2025-06-18 19:17:36 +0200

    modificado modelo, rutas y controlador de user y probando que todo funcione

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/UserController')
+const auth = require('../middlewares/auth');
 
 router.post('/', UserController.register);
 router.post('/login', UserController.login);
