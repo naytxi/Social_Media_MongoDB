e9fcfed003655cd5f7fc90950e9fcd7ca11d3a44
Nahia Otegui <nay.fullstack@gmail.com>
2025-06-18 18:50:34 +0200

    añado validaciones de creacion de usuarios

--- models/User.js
+++ models/User.js
@@ -1,14 +1,31 @@
 const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.Schema(
- {
-   name: String,
-   email: String,
-   password: String,
-   age: Number,
+const UserSchema = new mongoose.Schema({
+   name: {
+     type: String,
+     required: [true, 'Por favor rellena tu nombre'],
+   },
+   email: {
+     type: String,
+     match: [/.+\@.+\..+/, 'Este correo no es válido'],
+     unique: true,
+     required: [true, 'Por favor rellena tu correo'],
+   },
+   password: {
+     type: String,
+     required: [true, 'Por favor rellena tu contraseña'],
+   },
+   age: {
+     type: Number,
+     required: [true, 'Por favor rellena tu edad'],
+   },
+   role: String,
+   tokens: [],
+   orderIds: [{ type: ObjectId, ref: 'Order' }],
  },
  { timestamps: true }
 )
+
 
 const User = mongoose.model('User', UserSchema)
 
