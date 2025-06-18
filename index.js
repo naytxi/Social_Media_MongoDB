2928f313f01d513d1b92491fceeeaa846c621ac4
Nahia Otegui <nay.fullstack@gmail.com>
2025-06-18 20:06:30 +0200

    añado rutas, controlador y modelo post, probamos crear post

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express()
 const PORT = 5000
 const { dbConnection } = require('./config/config')
 const { typeError }= require('./middlewares/errors')
+const postRoutes = require('./routes/posts');
 
 app.use(express.json())
 dbConnection()
 
 app.use('/users', require('./routes/users'))
 app.use(typeError)
+
+app.use('/posts', postRoutes);
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
 
