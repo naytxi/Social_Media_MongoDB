86f0ba07b320c31e6bf4b35193a4d98fb941226b
Nahia Otegui <nay.fullstack@gmail.com>
2025-06-20 19:12:29 +0200

    getall and getbyid in postcontroller and post routes

--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,9 @@ const PostController = require('../controllers/PostController');
 const auth = require('../middlewares/auth');
 
 router.post('/', auth, PostController.create);
+router.put('/:id', auth, PostController.update);
+router.delete('/:id', auth, PostController.delete);
+router.get('/', PostController.getAll);
+router.get('/:id', PostController.getById);
 
 module.exports = router;
